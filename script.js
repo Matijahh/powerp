ff51cac8500cf7330a0b5505b46d12e10812c993
Matijahh <matijaradofficial@gmail.com>
2024-07-30 18:20:06 +0200

    Slider Animation - DONE

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function scrollServices(value) {
+  var firstRow = document.querySelector("#service1");
+  var secondRow = document.querySelector("#service2");
+  var thirdRow = document.querySelector("#service3");
+
+  if (value == 0) {
+    firstRow.classList.remove("blur");
+    secondRow.classList.add("blur");
+    thirdRow.classList.add("blur");
+  } else if (value == 50) {
+    firstRow.classList.add("blur");
+    secondRow.classList.remove("blur");
+    thirdRow.classList.add("blur");
+  } else if (value == 100) {
+    firstRow.classList.add("blur");
+    secondRow.classList.add("blur");
+    thirdRow.classList.remove("blur");
+  }
+}
