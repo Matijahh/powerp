97af09cd4ebf03370ff7580335e897b9db6e382f
Matijahh <matijaradofficial@gmail.com>
2024-08-01 10:47:43 +0200

    All Request Fixes - DONE

--- script.js
+++ script.js
@@ -6,21 +6,77 @@ function toggleMenu() {
 }
 
 function scrollServices(value) {
-  var firstRow = document.querySelector("#service1");
-  var secondRow = document.querySelector("#service2");
-  var thirdRow = document.querySelector("#service3");
+  var firstRowLeft = document.getElementById("left-service-1");
+  var secondRowLeft = document.getElementById("left-service-2");
+  var thirdRowLeft = document.getElementById("left-service-3");
+  var firstRowRight = document.getElementById("right-service-1");
+  var secondRowRight = document.getElementById("right-service-2");
+  var thirdRowRight = document.getElementById("right-service-3");
 
   if (value == 0) {
-    firstRow.classList.remove("blur");
-    secondRow.classList.add("blur");
-    thirdRow.classList.add("blur");
+    firstRowLeft.classList.remove("blur");
+    firstRowRight.classList.remove("blur");
+    secondRowLeft.classList.add("blur");
+    secondRowRight.classList.add("blur");
+    thirdRowLeft.classList.add("blur");
+    thirdRowRight.classList.add("blur");
   } else if (value == 50) {
-    firstRow.classList.add("blur");
-    secondRow.classList.remove("blur");
-    thirdRow.classList.add("blur");
+    firstRowLeft.classList.add("blur");
+    firstRowRight.classList.add("blur");
+    secondRowLeft.classList.remove("blur");
+    secondRowRight.classList.remove("blur");
+    thirdRowLeft.classList.add("blur");
+    thirdRowRight.classList.add("blur");
   } else if (value == 100) {
-    firstRow.classList.add("blur");
-    secondRow.classList.add("blur");
-    thirdRow.classList.remove("blur");
+    firstRowLeft.classList.add("blur");
+    firstRowRight.classList.add("blur");
+    secondRowLeft.classList.add("blur");
+    secondRowRight.classList.add("blur");
+    thirdRowLeft.classList.remove("blur");
+    thirdRowRight.classList.remove("blur");
   }
 }
+
+var sliderArea = document.querySelector(".products-services-content");
+sliderArea.addEventListener("wheel", function (e) {
+  var slider = document.getElementById("slider");
+  if (e.deltaY > 0) {
+    slider.valueAsNumber += 50;
+  } else {
+    slider.value -= 50;
+  }
+  e.preventDefault();
+  e.stopPropagation();
+
+  var firstRowLeft = document.getElementById("left-service-1");
+  var secondRowLeft = document.getElementById("left-service-2");
+  var thirdRowLeft = document.getElementById("left-service-3");
+  var firstRowRight = document.getElementById("right-service-1");
+  var secondRowRight = document.getElementById("right-service-2");
+  var thirdRowRight = document.getElementById("right-service-3");
+
+  console.log("First Row", firstRowLeft);
+
+  if (slider.value == 0) {
+    firstRowLeft.classList.remove("blur");
+    firstRowRight.classList.remove("blur");
+    secondRowLeft.classList.add("blur");
+    secondRowRight.classList.add("blur");
+    thirdRowLeft.classList.add("blur");
+    thirdRowRight.classList.add("blur");
+  } else if (slider.value == 50) {
+    firstRowLeft.classList.add("blur");
+    firstRowRight.classList.add("blur");
+    secondRowLeft.classList.remove("blur");
+    secondRowRight.classList.remove("blur");
+    thirdRowLeft.classList.add("blur");
+    thirdRowRight.classList.add("blur");
+  } else if (slider.value == 100) {
+    firstRowLeft.classList.add("blur");
+    firstRowRight.classList.add("blur");
+    secondRowLeft.classList.add("blur");
+    secondRowRight.classList.add("blur");
+    thirdRowLeft.classList.remove("blur");
+    thirdRowRight.classList.remove("blur");
+  }
+});
