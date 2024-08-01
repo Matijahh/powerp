2093b2319469ce88fcbbe8e7bf573c9b96da603a
Matijahh <matijaradofficial@gmail.com>
2024-08-01 18:40:13 +0200

    Mobile Version - Started

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+  const nav = document.querySelector(".hamburger-nav");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+  nav.classList.toggle("open");
 }
 
 function scrollServices(value) {
