75888870fe7466d8878d1c7f078c425b48fad6cf
Matijahh <matijaradofficial@gmail.com>
2024-08-18 20:14:33 +0200

    Slider Animation [FIXED]

--- script.js
+++ script.js
@@ -44,11 +44,17 @@ var sliderArea = document.querySelector(".products-services-content");
 sliderArea &&
   sliderArea.addEventListener("wheel", function (e) {
     var slider = document.getElementById("slider");
-    if (e.deltaY > 0) {
+
+    if (e.deltaY > 0 && slider.valueAsNumber === 100) {
+      window.scroll(window.scrollY + 100);
+    } else if (e.deltaY > 0 && slider.valueAsNumber !== 100) {
       slider.valueAsNumber += 50;
-    } else {
+    } else if (e.deltaY < 0 && slider.valueAsNumber === 0) {
+      window.scroll(window.scrollY - 100);
+    } else if (e.deltaY < 0 && slider.valueAsNumber !== 0) {
       slider.value -= 50;
     }
+
     e.preventDefault();
     e.stopPropagation();
 
@@ -58,8 +64,6 @@ sliderArea &&
     var firstRowRight = document.getElementById("right-service-1");
     var secondRowRight = document.getElementById("right-service-2");
     var thirdRowRight = document.getElementById("right-service-3");
-
-    console.log("First Row", firstRowLeft);
 
     if (slider.value == 0) {
       firstRowLeft.classList.remove("blur");
